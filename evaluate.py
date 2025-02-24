a7633417ac8ecf53226177181c4c5c39af12ff6e
Farzana Yasmin Ahmad <83102211+fa7sa@users.noreply.github.com>
2025-02-24 14:37:21 -0500

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -5,8 +5,6 @@ https://github.com/CaloChallenge/homepage/blob/main/code/
 """
 
 
-
-
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
@@ -23,210 +21,49 @@ import jetnet
 from pearson_frob import *
 from evaluate_range import *
 
-def file_read(file_name):
-    with h5py.File(file_name, "r") as h5f:
-        e = h5f['incident_energies'][::].astype(np.float32)  
-        shower = h5f['showers'][::].astype(np.float32)
-        #print(e.shape) 
-        #print(shower.shape)
-    return e, shower
 
 
-def extract_name_part(file_name):
-    # Use regular expression to extract the desired part of the filename
-    match = re.search(r'_([^_]+)\.h5(?:df5)?$', file_name)
-    if match:
-        return match.group(1)
-    else:
-        match = re.search(r'_([^_]+)\.hdf5$', file_name)
-        return match.group(1)
+
     
-
-def iterate_files(directory):
-    Es=[]
-    Showers=[]
-    model_names=[]
-    files=[]
-    for filename in os.listdir(directory):
-        if filename.endswith(".h5") or filename.endswith(".hdf5"):
-            file_path = os.path.join(directory, filename)
-            e,shower=file_read(file_path)
-            Es.append(e)
-            Showers.append(shower)
-            name_part = extract_name_part(filename)
-            files.append(file_path)
-            
-            if name_part:
-                #print("Extracted part from filename:", name_part)
-                model_names.append(name_part)
-    return model_names,Es,Showers,files
-
-def save_reference(ref_hlf, fname):
-    """ Saves high-level features class to file """
-    print("Saving file with high-level features.")
-    #filename = os.path.splitext(os.path.basename(ref_name))[0] + '.pkl'
-    with open(fname, 'wb') as file:
-        pickle.dump(ref_hlf, file)
-    print("Saving file with high-level features DONE.")
-    
-
-            
-def check_file(given_file, dataset, which=None):
-    """ checks if the provided file has the expected structure based on the dataset """
-    print("Checking if {} file has the correct form ...".format(
-        which if which is not None else 'provided'))
-    num_features = {'1-photons': 368, '1-pions': 533, '2': 6480, '3': 40500}[dataset]
-    num_events = given_file['incident_energies'].shape[0]
-    assert given_file['showers'].shape[0] == num_events, \
-        ("Number of energies provided does not match number of showers, {} != {}".format(
-            num_events, given_file['showers'].shape[0]))
-    assert given_file['showers'].shape[1] == num_features, \
-        ("Showers have wrong shape, expected {}, got {}".format(
-            num_features, given_file['showers'].shape[1]))
-
-    print("Found {} events in the file.".format(num_events))
-    print("Checking if {} file has the correct form: DONE \n".format(
-        which if which is not None else 'provided'))
-def extract_shower_and_energy(given_file, which):
-    """ reads .hdf5 file and returns samples and their energy """
-    print("Extracting showers from {} file ...".format(which))
-    shower = given_file['showers'][:]
-    energy = given_file['incident_energies'][:]
-    print("Extracting showers from {} file: DONE.\n".format(which))
-    return shower, energy
-def initialize_HLFs(path,particle,binning_file):
-    Es=[]
-    Showers=[]
-    
-    HLFs=[]
-    model_names,Es, Showers, files=iterate_files(path)
-
-    for i in range(len(model_names)):
-        hlf=HLF.HighLevelFeatures(particle,binning_file)
-        hlf.Einc=Es[i]
-        hlf.CalculateFeatures(Showers[i])
-        HLFs.append(hlf)
-    return Es, Showers, HLFs, model_names,files
-
-
-def prepare_high_data_for_classifier(hdf5_file, hlf_class, label):
-    """ takes hdf5_file, extracts high-level features, appends label, returns array """
-    voxel, E_inc = extract_shower_and_energy(hdf5_file, label)
-    nan_indices = np.where(np.isnan(voxel))
-    # print("in voxel: ", len(nan_indices[0]))
-    # print("in voxel: ", len(nan_indices[1]))
-          
-    #print("voxel shape: ",voxel.shape)
-    E_tot = hlf_class.GetEtot()
-    E_layer = []
-    for layer_id in hlf_class.GetElayers():
-        E_layer.append(hlf_class.GetElayers()[layer_id].reshape(-1, 1))
-    EC_etas = []
-    EC_phis = []
-    Width_etas = []
-    Width_phis = []
-    for layer_id in hlf_class.layersBinnedInAlpha:
-        EC_etas.append(hlf_class.GetECEtas()[layer_id].reshape(-1, 1))
-        EC_phis.append(hlf_class.GetECPhis()[layer_id].reshape(-1, 1))
-        Width_etas.append(hlf_class.GetWidthEtas()[layer_id].reshape(-1, 1))
-        Width_phis.append(hlf_class.GetWidthPhis()[layer_id].reshape(-1, 1))
-  
-    E_layer = np.concatenate(E_layer, axis=1)
-  
-    EC_etas = np.concatenate(EC_etas, axis=1)
-   
-    EC_phis = np.concatenate(EC_phis, axis=1)
-  
-    Width_etas = np.concatenate(Width_etas, axis=1)
-
-    Width_phis = np.concatenate(Width_phis, axis=1)
-    ret = np.concatenate([np.log10(E_inc), np.log10(E_layer+1e-8), EC_etas/1e2, EC_phis/1e2,
-                          Width_etas/1e2, Width_phis/1e2, label*np.ones_like(E_inc)], axis=1)
-    return ret
-
-def parse_arguments():
-    parser = configargparse.ArgumentParser(default_config_files=[])
-    parser.add_argument('--dataset_path', type=str, required=False, 
-                        default='/project/bi_dsc_community/calorimeter/calorimeter_evaluation_data/dataset_2/',
-                        help='path to generated h5/hdf5 files are stored')
-    parser.add_argument('--output_dir',type=str,
-                        default='results/')
-    parser.add_argument('--binning_file', type=str, required=False, 
-                        default='binning_dataset_2.xml',
-                        help='path to binning file')
-    parser.add_argument('--particle_type', type=str, required=True,default='electron',
-                        help='type of the particle being evaluated e.g., photon, pion, electron')
-    parser.add_argument('--dataset_num', type=int, required=True, default=2,
-                        help='dataset number e.g., 1, 2, 3')
-    parser.add_argument('--title_size', type=int, required=False, default=48,
-                        help='size of plot title')
-    parser.add_argument('--xlabel_size', type=int, required=False, default=44,
-                        help='size of xlabels')
-    parser.add_argument('--ylabel_size', type=int, required=False, default=44,
-                        help='size of ylabels')
-    parser.add_argument('--xtick_size', type=int, required=False, default=30,
-                        help='size of xtick')
-    parser.add_argument('--ytick_size', type=int, required=False, default=30,
-                        help='size of ytikc')
-    parser.add_argument('--legend_size',type=int,required=False,default=30,
-                       help='legend size')
-    parser.add_argument('--row',type=int,default=3,
-                       help='row size for subplot, for dataset 2 and 3 it should be 3, for pion it is 2 and photon it is 1')
-    parser.add_argument('--col',type=int,default=3,
-                       help='column size for subplot, for dataset 2 and 3 it should be 3, for pion and photon it is 2')
-    parser.add_argument('--metrics',type=str,required=False,default='all',
-                        help='type of metrics to evaluate'+\
-                        'all--is used for all high level features\' histograms such as layer dist, shower width, center of energy, sparsity, voxel dist, E_ratio'+\
-                        'sep-- is used to generate separation power graph'+\
-                        'emd-- is used to generate emd graph'+\
-                        'fpd-kpd-- is used to generate fpd,kpd values'+\
-                        'CFD --is used to generate pearson frobenius norm barplot and histogram'
-                        'layer-- is used to generate layer wise energy distribution.'
-                       )
-    parser.add_argument('--sep_file_path',type=str, help='path to file of separation and emd score')
-
-    args = parser.parse_args()
-    return args
-    
-def evaluate_metrics_ds_2_3(Es, Showers, HLFs, model_names, files, args):
+def evaluate_metrics_ds_2_3(Es, Showers, HLFs, model_names, files, args,model_to_color_dict):
     """Plot histograms for dataset 2 and 3 """
-    #compute layer wise energy distribution:
-    plot_layers(args)
+    #compute layer-wise energy distribution:
+    plot_layers(args,model_to_color_dict)
     ## pearson_frob_calc:
-    calc_CFD(Showers, model_names, args.output_dir,args.dataset_num)
+    calc_CFD(Showers, model_names, args.output_dir,args.dataset_num,model_to_color_dict)
     #sparsity
-    plot_sparsity_group(HLFs, args.dataset_num,args.output_dir,args.particle_type, model_names, width=4,height=3,
+    plot_sparsity_group(HLFs, args.dataset_num,args.output_dir,args.particle_type, model_names,model_to_color_dict, width=4,height=3,
                         TITLE_SIZE=args.title_size,XLABEL_SIZE=args.xlabel_size,YLABEL_SIZE=args.ylabel_size,
                                 LEGEND_SIZE=args.legend_size,XTICK_SIZE=args.xtick_size,YTICK_SIZE=args.ytick_size)
     min_energy=0.5e-3/0.033
     #voxel energy dist
-    plot_cell_dist(Showers,min_energy,args.dataset_num,args.output_dir, args.particle_type, model_names, 
+    plot_cell_dist(Showers,min_energy,args.dataset_num,args.output_dir, args.particle_type, model_names,model_to_color_dict, 
                       ratio = False,height=6,width=8,YMAX=100,LEGEND_SIZE=args.legend_size,
                       XLABEL_SIZE=args.xlabel_size,YLABEL_SIZE=args.ylabel_size,
                       TITLE_SIZE=args.title_size,XTICK_SIZE=args.xtick_size,YTICK_SIZE=args.ytick_size)
     #E_ratio
-    plot_Etot_Einc_new(HLFs, args.dataset_num,args.output_dir, args.particle_type, model_names, 
+    plot_Etot_Einc_new(HLFs, args.dataset_num,args.output_dir, args.particle_type, model_names,model_to_color_dict, 
                       row=1,col=1,height=6,width=8,YMAX=20,LEGEND_SIZE=args.legend_size,
                       XLABEL_SIZE=args.xlabel_size,YLABEL_SIZE=args.ylabel_size,
                       TITLE_SIZE=args.title_size,XTICK_SIZE=args.xtick_size,YTICK_SIZE=args.ytick_size)
     #center of energy in eta direction
-    plot_ECEtas_group(HLFs, args.dataset_num,args.output_dir, args.particle_type, model_names, 
+    plot_ECEtas_group(HLFs, args.dataset_num,args.output_dir, args.particle_type, model_names, model_to_color_dict,
                       ratio = False,row=3,col=3,height=6,width=8,YMAX=100,LEGEND_SIZE=args.legend_size,
                       XLABEL_SIZE=args.xlabel_size,YLABEL_SIZE=args.ylabel_size,
                       TITLE_SIZE=args.title_size,XTICK_SIZE=args.xtick_size,YTICK_SIZE=args.ytick_size)
 
     #center of energy in phi direction
-    plot_ECPhis_group(HLFs, args.dataset_num,args.output_dir, args.particle_type, model_names, 
+    plot_ECPhis_group(HLFs, args.dataset_num,args.output_dir, args.particle_type, model_names, model_to_color_dict,
                       ratio = False,row=3,col=3,height=6,width=8,YMAX=100,LEGEND_SIZE=args.legend_size,
                       XLABEL_SIZE=args.xlabel_size,YLABEL_SIZE=args.ylabel_size,
                       TITLE_SIZE=args.title_size,XTICK_SIZE=args.xtick_size,YTICK_SIZE=args.ytick_size)
     #shower width in eta
-    plot_SW_etas_group(HLFs, args.dataset_num,args.output_dir, args.particle_type, model_names, 
+    plot_SW_etas_group(HLFs, args.dataset_num,args.output_dir, args.particle_type, model_names, model_to_color_dict,
                       ratio = False,row=3,col=3,height=6,width=8,YMAX=100,LEGEND_SIZE=args.legend_size,
                       XLABEL_SIZE=args.xlabel_size,YLABEL_SIZE=args.ylabel_size,
                       TITLE_SIZE=args.title_size,XTICK_SIZE=args.xtick_size,YTICK_SIZE=args.ytick_size)
     #shower width in phi
-    plot_SW_Phis_group(HLFs, args.dataset_num,args.output_dir, args.particle_type, model_names, 
+    plot_SW_Phis_group(HLFs, args.dataset_num,args.output_dir, args.particle_type, model_names, model_to_color_dict, 
                       ratio = False,row=3,col=3,height=6,width=8,YMAX=100,LEGEND_SIZE=args.legend_size,
                       XLABEL_SIZE=args.xlabel_size,YLABEL_SIZE=args.ylabel_size,
                       TITLE_SIZE=args.title_size,XTICK_SIZE=args.xtick_size,YTICK_SIZE=args.ytick_size)
@@ -321,11 +158,12 @@ def main():
     if args.metrics=='all':
          ## this returns incident energy, showers, HLFs object for each sample dataset and their order in the folder
         Es,Showers,HLFs,model_names,files=initialize_HLFs(args.dataset_path,args.particle_type,args.binning_file)
+        model_to_color_dict=create_model_to_color_dict(model_names)
     
         if args.dataset_num==2 or args.dataset_num== 3:
             #computing fpd_kpd
             evaluate_fpd_kpd(Es,Showers,HLFs,model_names,files,args)
-            evaluate_metrics_ds_2_3(Es, Showers, HLFs, model_names, files, args)
+            evaluate_metrics_ds_2_3(Es, Showers, HLFs, model_names, files, args,model_to_color_dict)
             
         elif args.dataset_num==1:
             evaluate_metrics_ds_1(Es, Showers, HLFs, model_names, files, args)
@@ -344,7 +182,10 @@ def main():
         Es,Showers,HLFs,model_names,files=initialize_HLFs(args.dataset_path,args.particle_type,args.binning_file)
         evaluate_fpd_kpd(Es,Showers,HLFs,model_names,files,args)
     elif args.metrics=='CFD':
-        gen_CFD(args.dataset_path,args.dataset_num,args.output_dir)
+        Es,Showers,HLFs,model_names,files=initialize_HLFs(args.dataset_path,args.particle_type,args.binning_file)
+        model_to_color_dict=create_model_to_color_dict(model_names)
+        calc_CFD(Showers, model_names, args.output_dir,args.dataset_num,model_to_color_dict)
+        
     elif args.metrics=='layer':
         plot_layers(args)
         
